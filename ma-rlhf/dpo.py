f0c5c0999c1eb11fa365ed40e3a6cc17a794ba0b
denghang <82824374@qq.com>
2024-01-22 19:53:43 +0800

    add dpo debug version

---
+++ ma-rlhf/dpo.py
@@ -0,0 +1,214 @@
+from datasets import load_dataset, load_from_disk
+from trl import DPOTrainer
+import re
+import torch
+import evaluate
+from accelerate import Accelerator
+from utils import (
+    create_peft_reward_model,
+    ScriptArguments,
+    DEFINE_EOS_TOKEN,
+)
+from transformers import (
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    AutoModelForSequenceClassification,
+    TrainingArguments
+)
+
+parser = HfArgumentParser(ScriptArguments)
+train_args: ScriptArguments = parser.parse_args_into_dataclasses(return_remaining_strings=True)[0]
+
+dataset_name = train_args.dataset_name
+model_name = train_args.model_name
+deepspeed_config_name = train_args.deepspeed_config_name
+output_max_length = train_args.output_max_length
+seq_length = train_args.seq_length
+batch_size = train_args.batch_size
+output_name = train_args.output_name
+is_peft = train_args.use_QLora
+is_use_flash_attention2 = train_args.use_flash_attention_2
+num_train_epochs = train_args.num_train_epochs
+beta = 0.1 # default
+
+
+accuracy = evaluate.load("accuracy")
+def compute_metrics(eval_pred):
+    predictions, _ = eval_pred
+    # Here, predictions is rewards_j and rewards_k.
+    # We want to see how much of the time rewards_j > rewards_k.
+    predictions = np.argmax(predictions, axis=0)
+    labels = np.zeros(predictions.shape)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+def create_model_tokenizer(name):
+    # QLoRA
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    device_map = {"": Accelerator().local_process_index}
+    print('device map: ', device_map)
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name, quantization_config=bnb_config, device_map=device_map, num_labels=1
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, model_max_length=seq_length)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.eos_token = DEFINE_EOS_TOKEN
+    # https://stackoverflow.com/questions/68084302/assertionerror-cannot-handle-batch-sizes-1-if-no-padding-token-is-defined
+    model.config.pad_token_id = model.config.eos_token_id
+
+    return model, tokenizer
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.eos_token = DEFINE_EOS_TOKEN
+
+# for prompt, chosen, rejected
+def preprocess_function(examples):
+    new_examples = {
+        "input_ids_chosen": [],
+        "attention_mask_chosen": [],
+        "input_ids_rejected": [],
+        "attention_mask_rejected": [],
+    }
+    for question, response_j, response_k in zip(
+        examples["question"], examples["response_chosen"], examples["response_rejected"]
+    ):
+        tokenized_j = tokenizer(
+            f"###Question:{question}\n###Answer:{response_j}{tokenizer.eos_token}",
+            truncation=True,
+        )
+        tokenized_k = tokenizer(
+            f"###Question:{question}\n###Answer:{response_k}{tokenizer.eos_token}",
+            truncation=True,
+        )
+
+        new_examples["input_ids_chosen"].append(tokenized_j["input_ids"])
+        new_examples["attention_mask_chosen"].append(tokenized_j["attention_mask"])
+        new_examples["input_ids_rejected"].append(tokenized_k["input_ids"])
+        new_examples["attention_mask_rejected"].append(tokenized_k["attention_mask"])
+
+    return new_examples
+
+
+# Anthropic/hh-rlhf
+# chosen, rejected
+def preprocess_function_hhrlhf(examples):
+    new_examples = {
+        "prompt": [],
+        "chosen": [],
+        "rejected": [],
+    }
+
+
+    for prompt_chosen, prompt_rejected in zip(
+        examples["chosen"], examples["rejected"]
+    ):
+
+        prompt_chosen = re.sub(r'Human:', '###Question:', prompt_chosen)
+        prompt_chosen = re.sub(r'Assistant:', '\n###Answer:', prompt_chosen)
+        prompt_rejected = re.sub(r'Human:', '###Question:', prompt_rejected)
+        prompt_rejected = re.sub(r'Assistant:', '\n###Answer:', prompt_rejected)
+
+
+        prompt_question = prompt_chosen.split('\n###Answer:',1)[0] + '\n###Answer:'
+        response_chosen = prompt_chosen.split('\n###Answer:',1)[1]
+        response_rejected = prompt_rejected.split('\n###Answer:',1)[1]
+
+        new_examples['prompt'].append(prompt_question)
+        new_examples['chosen'].append(response_chosen)
+        new_examples['rejected'].append(rejected)
+
+
+    return new_examples
+
+
+def create_dpo_datasets(datasets_name, dataset_sub_name, tokenizer):
+    train_dataset = load_dataset(datasets_name, split='train')
+    eval_dataset = load_dataset(datasets_name, split='test')
+
+    train_dataset = train_dataset.map(
+        preprocess_function_hhrlhf,
+        batched=True,
+        num_proc=8,
+    )
+
+    torch.distributed.barrier()
+    train_dataset = train_dataset.filter(
+        lambda x: len(x["input_ids_chosen"]) <= seq_length
+        and len(x["input_ids_rejected"]) <= seq_length
+    )
+    torch.distributed.barrier()
+
+    eval_dataset = eval_dataset.map(
+        preprocess_function_hhrlhf,
+        batched=True,
+        num_proc=8,
+    )
+
+    torch.distributed.barrier()
+    eval_dataset = eval_dataset.filter(
+        lambda x: len(x["input_ids_chosen"]) <= seq_length
+        and len(x["input_ids_rejected"]) <= seq_length
+    )
+
+    torch.distributed.barrier()
+
+    return train_dataset, eval_dataset
+
+
+def train():
+    model, tokenizer = create_model_tokenizer(model_name)  # model is sequence classification
+    train_datasets, test_datasets = create_dpo_datasets(
+        dataset_name, None, tokenizer
+    )
+
+    # PEFT
+    peft_config = create_dpo_model(is_peft)
+
+    training_args = TrainingArguments(
+        output_dir=output_name,
+        push_to_hub=False,
+        # save_strategy='epoch',
+        logging_steps=1,
+        num_train_epochs=1,
+        gradient_checkpointing=True,
+        bf16=True,
+        learning_rate=2e-5,
+        warmup_ratio=0.1,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        gradient_accumulation_steps=1,
+        deepspeed=deepspeed_config_name,
+        report_to='wandb',
+        lr_scheduler_type='cosine',
+        max_steps=10,
+    )
+
+    trainer = DPOTrainer(
+        model,
+        None,
+        args=training_args,
+        beta=beta,
+        train_dataset=train_datasets,
+        eval_dataset=test_datasets,
+        tokenizer=tokenizer,
+        peft_config=peft_config,
+        max_prompt_length= output_max_length,
+        max_length= seq_length,
+    )
+
+
+    trainer.train()
+    trainer.save_model(output_name)
+
+
+if __name__ == "__main__":
+    train()
---
+++ scripts/run_all_7b_dpo.sh
@@ -0,0 +1,87 @@
+# 完整运行
+
+base_model_path='meta-llama/Llama-2-7b-hf'
+deepspeed_config_name=./config/ds.json
+output_path='./output'
+
+model_pretrained_lora_path=${output_path}'/pretrained_lora'
+model_pretrained_full_path=${output_path}'/pretrained_full'
+model_sft_lora_path=${output_path}'/sft_lora'
+model_sft_full_path=${output_path}'/sft_full'
+model_reward_model_lora_path=${output_path}'/reward_model_lora'
+model_ppo_lora_path=${output_path}'/ppo_lora'
+model_ppo_full_path=${output_path}'/ppo_full'
+model_dpo_lora_path=${output_path}'/dpo_lora'
+model_dpo_full_path=${output_path}'/dpo_full'
+
+
+
+echo '-------------------------------------------------------'
+date
+echo '-------------------------------------------------------'
+
+# # stage: sft
+# sft_dataset_name='yahma/alpaca-cleaned'
+# model_pretrained_full_path=${base_model_path}
+# deepspeed ./ma-rlhf/sft.py \
+# 	--dataset_name=${sft_dataset_name} \
+# 	--model_name=${model_pretrained_full_path} \
+# 	--seq_length=1024 \
+# 	--output_name=${model_sft_lora_path} \
+# 	--use_QLora=True \
+# 	--batch_size=16 \
+# 	--use_flash_attention_2=True \
+# 	--deepspeed_config_name=${deepspeed_config_name} \
+# 	--num_train_epochs=2
+
+
+# # merge SFT
+# python ./ma-rlhf/merge_adapter.py \
+# 	--base_model_name=${model_pretrained_full_path} \
+# 	--model_name=${model_sft_lora_path} \
+# 	--merged_model_name=${model_sft_full_path}
+
+
+# /root/Projects/MA-RLHF/output/reward_model_lora/checkpoint-500
+# stage dpo
+rm_dataset_name='Anthropic/hh-rlhf'
+deepspeed ./ma-rlhf/dpo.py \
+	--dataset_name=${rm_dataset_name} \
+	--model_name=${model_sft_full_path} \
+	--output_name=${model_dpo_lora_path} \
+	--use_QLora=True \
+	--use_flash_attention_2=True \
+	--deepspeed_config_name=${deepspeed_config_name} \
+	--batch_size=4 \
+	--mini_batch_size=2 \
+	--num_train_epochs=1 \
+	--output_max_length=256 \
+	--seq_length=1024
+
+
+# merge PPO
+python ./ma-rlhf/merge_adapter.py \
+	--base_model_name=${model_sft_full_path} \
+	--model_name=${model_dpo_lora_path} \
+	--merged_model_name=${model_dpo_full_path}
+
+
+
+# generate result
+echo "------------------print sft result------------------"
+python ./ma-rlhf/generate.py \
+	--model_name=${model_sft_full_path} \
+	--prompt='give me a C++ code about quick sort?' \
+	--max_new_tokens=256
+
+
+echo "------------------print dpo result------------------"
+python ./ma-rlhf/generate.py \
+	--model_name=${model_dpo_full_path} \
+	--prompt='how to make a bomb?' \
+	--max_new_token=256
+
+
+echo '-------------------------------------------------------'
+date
+echo '-------------------------------------------------------'
