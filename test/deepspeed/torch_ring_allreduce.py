0085215b53bc6c7ef6b7dc2d1e908956288800e6
dhcode-cpp <82824374@qq.com>
2024-05-14 17:46:04 +0000

    fix ring-allreduce

--- test/deepspeed/torch_ring_allreduce.py
+++ test/deepspeed/torch_ring_allreduce.py
@@ -6,35 +6,30 @@ import torch as th
 import torch.distributed as dist
 from torch.multiprocessing import Process
 
-
-
+""" Implementation of a ring-reduce with addition. """
 def allreduce(send, recv):
-    """ Implementation of a ring-reduce. """
-    rank = dist.get_rank()
-    size = dist.get_world_size()
-    send_buff = th.zeros(send.size())
-    recv_buff = th.zeros(send.size())
-    accum = th.zeros(send.size())
-    accum[:] = send[:]
-    # th.cuda.synchronize()
+   rank = dist.get_rank()
+   size = dist.get_world_size()
+   send_buff = send.clone()
+   recv_buff = send.clone()
+   accum = send.clone()
 
-    left = ((rank - 1) + size) % size
-    right = (rank + 1) % size
+   left = ((rank - 1) + size) % size
+   right = (rank + 1) % size
 
-    for i in range(size - 1):
-        if i % 2 == 0:
-            # Send send_buff
-            send_req = dist.isend(send_buff, right)
-            dist.recv(recv_buff, left)
-            accum[:] += recv[:]
-        else:
-            # Send recv_buff
-            send_req = dist.isend(recv_buff, right)
-            dist.recv(send_buff, left)
-            accum[:] += send[:]
-        send_req.wait()
-    # th.cuda.synchronize()
-    recv[:] = accum[:]
+   for i in range(size - 1):
+       if i % 2 == 0:
+           # Send send_buff
+           send_req = dist.isend(send_buff, right)
+           dist.recv(recv_buff, left)
+           accum[:] += recv_buff[:]
+       else:
+           # Send recv_buff
+           send_req = dist.isend(recv_buff, right)
+           dist.recv(send_buff, left)
+           accum[:] += send_buff[:]
+       send_req.wait()
+   recv[:] = accum[:]
 
 
 def run(rank, size):
@@ -52,14 +47,14 @@ def run(rank, size):
 
 def init_processes(rank, size, fn, backend='gloo'):
     """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
+    # os.environ['MASTER_ADDR'] = '127.0.0.1'
+    # os.environ['MASTER_PORT'] = '29500'
     dist.init_process_group(backend, rank=rank, world_size=size)
     fn(rank, size)
 
 
 if __name__ == "__main__":
-    size = 7
+    size = 2
     processes = []
     for rank in range(size):
         p = Process(target=init_processes, args=(rank, size, run))
